8d311a2147f53342d80601941399cb5678a1e6f7
EmanuelaPau <uapame@gmail.com>
2023-07-05 15:24:25 +0200

    Add vue and axios

--- index.php
+++ index.php
@@ -7,12 +7,20 @@
     <title>Albums</title>
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <!-- Vite -->
-
+    <!-- Vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- Axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"></script>
+    <!-- CSS style -->
+    <link rel="stylesheet" href="style/style.css">
 </head>
 
 <body>
 
+    <div id="app">
+
+    </div>
+    <script src="./script/script.js"></script>
 </body>
 
 </html>
