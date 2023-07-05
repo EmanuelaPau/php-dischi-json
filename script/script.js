8827d9303d144336a0b063b40e71a5cbd20b2320
EmanuelaPau <uapame@gmail.com>
2023-07-05 15:53:05 +0200

    recieve axios call in index.php

--- script/script.js
+++ script/script.js
@@ -9,11 +9,23 @@ createApp({
 
     methods: {
         getItems() {
+            axios.get(this.apiUrl, {
+                params: {}
+            })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
 
         }
     },
 
     created() {
-
+        this.getItems();
     },
 }).mount('#app')
